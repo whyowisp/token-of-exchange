78397cce3a8d8dcc53cd7ce0456f9a1f1dac7e58
whyowisp <mikko.sipola@tutanota.com>
2025-06-02 17:05:16 +0300

    theme update

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,3 @@
-
 import { createTheme } from '@mui/material';
 
 const theme = createTheme({
@@ -11,6 +10,15 @@ const theme = createTheme({
       main: '#475569',   // Slate Blue (mid)
       dark: '#1E293B',   // Charcoal Blue
       contrastText: '#ffffff',
+    },
+    thriving: {
+      main: '#6DBF75',
+    },
+    deprived: {
+      main: '#D97706',
+    },
+    deceased: {
+      main: '#4F4F4F',
     },
     background: {
       paper: 'rgba(255, 255, 255, 0.8)',
@@ -26,6 +34,17 @@ const darkTheme = createTheme({
     dark: true,
   },
   palette: {
+    thriving: {
+      main: '#6DBF75',
+    },
+    deprived: {
+      main: '#D97706',
+    },
+    deceased: {
+      main: '#1F1F1F'
+    },
+
+
     background: {
       paper: 'rgba(0, 0, 0, 0.8)',
       default: 'rgba(0, 0, 0, 0.8)',
